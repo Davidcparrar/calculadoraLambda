96abb7583a274620506d3d25353f69da0f234d2e
David Parra <davidc.parrar@gmail.com>
2024-03-21 18:41:53 -0500

    test

--- calculator.py
+++ calculator.py
@@ -6,8 +6,8 @@ def lambda_handler(event, context):
         f"Sum of {number_1} and {number_2} is {int(number_1) + int(number_2)}"
     )
 
-    return {"status": "success", "result": int(number_1) + int(number_2)}
+    return {"status": "success!", "result": int(number_1) + int(number_2)}
 
 
 if __name__ == "__main__":
-    lambda_handler({"number_1": "5", "number_2": "10"}, None)
+    lambda_handler({"number_1": "10", "number_2": "10"}, None)
