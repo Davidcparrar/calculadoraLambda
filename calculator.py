48caa55415ae5b6fb8ab82ff55317f620dd47b83
David Parra <davidc.parrar@gmail.com>
2024-03-21 17:49:16 -0500

    fix: calculator formatting

--- calculator.py
+++ calculator.py
@@ -1,9 +1,10 @@
 def lambda_handler(event, context):
-    
-    number_1 = event.get('number_1', "5")
-    number_2 = event.get('number_2', "10")
+    number_1 = event.get("number_1", "5")
+    number_2 = event.get("number_2", "10")
 
-    print(f"Sum of {number_1} and {number_2} is {int(number_1) + int(number_2)}")
+    print(
+        f"Sum of {number_1} and {number_2} is {int(number_1) + int(number_2)}"
+    )
 
     return {"status": "success", "result": int(number_1) + int(number_2)}
 
