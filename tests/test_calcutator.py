3582259d0f74a211c4dacf9241c5b53c2c5951f5
David Parra <davidc.parrar@gmail.com>
2024-03-21 18:58:38 -0500

    feat: add multiply function

--- tests/test_calcutator.py
+++ tests/test_calcutator.py
@@ -10,3 +10,16 @@ def test_add():
     assert add(0, 1) == 1
     assert add(0, -1) == -1
     assert add(-1, 0) == -1
+
+def test_multiply():
+    from src.calculator import multiply
+    assert multiply(1, 2) == 2
+    assert multiply(0, 0) == 0
+    assert multiply(-1, -1) == 1
+    assert multiply(-1, 1) == -1
+    assert multiply(1, -1) == -1
+    assert multiply(1, 0) == 0
+    assert multiply(0, 1) == 0
+    assert multiply(0, -1) == 0
+    assert multiply(-1, 0) == 0
+    assert multiply(1224, 2) == 2448
