a04ab4726f5381c98c2bd97b38c040deb9516612
David Parra <davidc.parrar@gmail.com>
2024-03-21 19:02:33 -0500

    feat: add multiply functionality

--- calculator.py
+++ calculator.py
@@ -1,13 +1,20 @@
-def lambda_handler(event, context):
+from src.calculator import add, multiply
+
+
+def lambda_handler(event: dict, context: dict) -> dict:
+    operation = event.get("operation", "add")
+
     number_1 = event.get("number_1", "5")
     number_2 = event.get("number_2", "10")
 
-    print(
-        f"Sum of {number_1} and {number_2} is {int(number_1) + int(number_2)}"
-    )
+    result = None
+    if operation == "add":
+        result = add(int(number_1), int(number_2))
+    elif operation == "multiply":
+        result = multiply(int(number_1), int(number_2))
 
-    return {"status": "success!", "result": int(number_1) + int(number_2)}
+    return {"status": "success", "operation": operation, "result": result}
 
 
 if __name__ == "__main__":
-    lambda_handler({"number_1": "10", "number_2": "10"}, None)
+    lambda_handler({"number_1": "10", "number_2": "10"}, {})
